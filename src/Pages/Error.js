f1672a617f6868e5fb6638d5e780d9441cac6f29
quppiehy <sunhuiyi@gmail.com>
2023-08-27 21:40:34 +0800

    added error page and button theme, grid centered theme

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,39 +1,74 @@
 import React from "react";
-import { Box, Button, Container, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Typography,
+  ThemeProvider,
+  Stack,
+  styled,
+} from "@mui/material";
 import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
 import error from "../Assets/Images/error.png";
+import { theme } from "../Assets/Styles/Theme";
 
 export default function Error() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-      }}
-    >
-      <Container maxWidth="md">
-        <Grid container spacing={2}>
-          <Grid xs={6}>
-            <Typography variant="h1">404</Typography>
-            <Typography variant="h6">
-              So sorry! The page you’re looking for doesn’t exist.
-              <br />
-              Please let us show you the way back! Or, feel free to search jobs
-              listed!
-            </Typography>
-            <Link to="/">
-              <Button variant="contained">Search Jobs</Button>
-              <Button variant="contained">Go Back Home</Button>
-            </Link>
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          ...theme.customStyles.centered.container,
+          minHeight: "100vh",
+        }}
+      >
+        <Container maxWidth="md">
+          <Grid
+            container
+            spacing={2}
+            direction="row"
+            sx={theme.customStyles.centered.container}
+          >
+            <Grid item xs={6}>
+              <Typography
+                variant="h1"
+                sx={{ fontWeight: theme.typography.fontWeightBold }}
+              >
+                404
+              </Typography>
+              <Typography variant="h6">
+                The page you’re looking for doesn’t exist.
+                <br />
+                Please let us show you the way back, <br />
+                or feel free to search jobs listed!
+              </Typography>
+
+              <Stack direction="row" spacing={3}>
+                <Button
+                  classes={{ root: "blue" }}
+                  variant="contained"
+                  component={Link}
+                  to="/search"
+                >
+                  Search Jobs
+                </Button>
+
+                <Button
+                  classes={{ root: "blue" }}
+                  variant="contained"
+                  component={Link}
+                  to="/"
+                >
+                  Go Back Home
+                </Button>
+              </Stack>
+            </Grid>
+            <Grid item xs={6}>
+              <img src={error} alt="error" />
+            </Grid>
           </Grid>
-          <Grid xs={6}>
-            <img src={error} alt="error" width={500} height={250} />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
