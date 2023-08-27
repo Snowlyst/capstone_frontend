1700190bef1358b96c885ce44637c1ff2fd54b2e
quppiehy <sunhuiyi@gmail.com>
2023-08-27 18:01:48 +0800

    added error page

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,11 +1,39 @@
-import { Box } from "@mui/material";
+import React from "react";
+import { Box, Button, Container, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import { Link } from "react-router-dom";
+import error from "../Assets/Images/error.png";
 
-function Error() {
+export default function Error() {
   return (
-    <Box>
-      <Box>YOU COME TO THE WRONG NEIGHBOURHOOD</Box>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Container maxWidth="md">
+        <Grid container spacing={2}>
+          <Grid xs={6}>
+            <Typography variant="h1">404</Typography>
+            <Typography variant="h6">
+              So sorry! The page you’re looking for doesn’t exist.
+              <br />
+              Please let us show you the way back! Or, feel free to search jobs
+              listed!
+            </Typography>
+            <Link to="/">
+              <Button variant="contained">Search Jobs</Button>
+              <Button variant="contained">Go Back Home</Button>
+            </Link>
+          </Grid>
+          <Grid xs={6}>
+            <img src={error} alt="error" width={500} height={250} />
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
   );
 }
-
-export default Error;
